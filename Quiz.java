2bac682f756a62ef900989cbfd3a5dad70fc666c
Paaras Sharma <90600305+Paaras1999@users.noreply.github.com>
2024-01-27 03:09:21 +0530

    Add files via upload

--- Quiz.java
+++ Quiz.java
@@ -2,34 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Quiz extends JFrame implements ActionListener {    
-    // Declaration of instance variables
+public class Quiz extends JFrame implements ActionListener {
+    
     String questions[][] = new String[10][5];
     String answers[][] = new String[10][2];
     String useranswers[][] = new String[10][1];
-    String name;
-    // GUI components
     JLabel qno, question;
     JRadioButton opt1, opt2, opt3, opt4;
     ButtonGroup groupoptions;
     JButton next, submit, lifeline;
-    // Timer and score variables
+    
     public static int timer = 15;
     public static int ans_given = 0;
     public static int count = 0;
     public static int score = 0;
-    // Constructor to initialize the quiz
+    
+    String name;
+    
     Quiz(String name) {
         this.name = name;
-        setBounds(50, 0, 1440, 850); // Setting the JFrame bounds
-        getContentPane().setBackground(Color.WHITE); // Setting background color
-        setLayout(null); // Using null layout
-        // Loading image for the quiz
+        setBounds(50, 0, 1440, 850);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/quiz.jpg"));
         JLabel image = new JLabel(i1);
         image.setBounds(0, 0, 1440, 392);
         add(image);
-        // Initializing JLabels for question number and question text
+        
         qno = new JLabel();
         qno.setBounds(100, 450, 50, 30);
         qno.setFont(new Font("Tahoma", Font.PLAIN, 24));
@@ -40,16 +40,77 @@ public class Quiz extends JFrame implements ActionListener {
         question.setFont(new Font("Tahoma", Font.PLAIN, 24));
         add(question);
         
-        // Adding questions and answers
-        // Each question has 4 options
-        // questions[i][0] contains the question text
-        // questions[i][1] to questions[i][4] contain the options
-        // answers[i][1] contains the correct answer for question i
-        // answers[i][0] is not used in this code
-        // Similar structure for useranswers
-        // You can add more questions and options by extending the array size
+        questions[0][0] = "Which is used to find and fix bugs in the Java programs.?";
+        questions[0][1] = "JVM";
+        questions[0][2] = "JDB";
+        questions[0][3] = "JDK";
+        questions[0][4] = "JRE";
+
+        questions[1][0] = "What is the return type of the hashCode() method in the Object class?";
+        questions[1][1] = "int";
+        questions[1][2] = "Object";
+        questions[1][3] = "long";
+        questions[1][4] = "void";
+
+        questions[2][0] = "Which package contains the Random class?";
+        questions[2][1] = "java.util package";
+        questions[2][2] = "java.lang package";
+        questions[2][3] = "java.awt package";
+        questions[2][4] = "java.io package";
+
+        questions[3][0] = "An interface with no fields or methods is known as?";
+        questions[3][1] = "Runnable Interface";
+        questions[3][2] = "Abstract Interface";
+        questions[3][3] = "Marker Interface";
+        questions[3][4] = "CharSequence Interface";
+
+        questions[4][0] = "In which memory a String is stored, when we create a string using new operator?";
+        questions[4][1] = "Stack";
+        questions[4][2] = "String memory";
+        questions[4][3] = "Random storage space";
+        questions[4][4] = "Heap memory";
+
+        questions[5][0] = "Which of the following is a marker interface?";
+        questions[5][1] = "Runnable interface";
+        questions[5][2] = "Remote interface";
+        questions[5][3] = "Readable interface";
+        questions[5][4] = "Result interface";
+
+        questions[6][0] = "Which keyword is used for accessing the features of a package?";
+        questions[6][1] = "import";
+        questions[6][2] = "package";
+        questions[6][3] = "extends";
+        questions[6][4] = "export";
+
+        questions[7][0] = "In java, jar stands for?";
+        questions[7][1] = "Java Archive Runner";
+        questions[7][2] = "Java Archive";
+        questions[7][3] = "Java Application Resource";
+        questions[7][4] = "Java Application Runner";
+
+        questions[8][0] = "Which of the following is a mutable class in java?";
+        questions[8][1] = "java.lang.StringBuilder";
+        questions[8][2] = "java.lang.Short";
+        questions[8][3] = "java.lang.Byte";
+        questions[8][4] = "java.lang.String";
+
+        questions[9][0] = "Which of the following option leads to the portability and security of Java?";
+        questions[9][1] = "Bytecode is executed by JVM";
+        questions[9][2] = "The applet makes the Java code secure and portable";
+        questions[9][3] = "Use of exception handling";
+        questions[9][4] = "Dynamic binding between objects";
         
-        // Initializing radio buttons and button group
+        answers[0][1] = "JDB";
+        answers[1][1] = "int";
+        answers[2][1] = "java.util package";
+        answers[3][1] = "Marker Interface";
+        answers[4][1] = "Heap memory";
+        answers[5][1] = "Remote interface";
+        answers[6][1] = "import";
+        answers[7][1] = "Java Archive";
+        answers[8][1] = "java.lang.StringBuilder";
+        answers[9][1] = "Bytecode is executed by JVM";
+        
         opt1 = new JRadioButton();
         opt1.setBounds(170, 520, 700, 30);
         opt1.setBackground(Color.WHITE);
@@ -74,14 +135,12 @@ public class Quiz extends JFrame implements ActionListener {
         opt4.setFont(new Font("Dialog", Font.PLAIN, 20));
         add(opt4);
         
-        // Adding radio buttons to a button group
         groupoptions = new ButtonGroup();
         groupoptions.add(opt1);
         groupoptions.add(opt2);
         groupoptions.add(opt3);
         groupoptions.add(opt4);
         
-        // Adding buttons for next, 50-50 lifeline, and submit
         next = new JButton("Next");
         next.setBounds(1100, 550, 200, 40);
         next.setFont(new Font("Tahoma", Font.PLAIN, 22));
@@ -105,15 +164,14 @@ public class Quiz extends JFrame implements ActionListener {
         submit.setForeground(Color.WHITE);
         submit.addActionListener(this);
         submit.setEnabled(false);
-        add(submit);   
-        // Starting the quiz
+        add(submit);
+        
         start(count);
         
-        setVisible(true); // Making the frame visible
+        setVisible(true);
     }
-    // ActionListener implementation for button clicks
+    
     public void actionPerformed(ActionEvent ae) {
-        // Handling actions for next button
         if (ae.getSource() == next) {
             repaint();
             opt1.setEnabled(true);
@@ -135,9 +193,7 @@ public class Quiz extends JFrame implements ActionListener {
             
             count++;
             start(count);
-        }
-        // Handling actions for 50-50 lifeline button
-        else if (ae.getSource() == lifeline) {
+        } else if (ae.getSource() == lifeline) {
             if (count == 2 || count == 4 || count == 6 || count == 8 || count == 9) {
                 opt2.setEnabled(false);
                 opt3.setEnabled(false);
@@ -146,16 +202,14 @@ public class Quiz extends JFrame implements ActionListener {
                 opt4.setEnabled(false);
             }
             lifeline.setEnabled(false);
-        }
-        // Handling actions for submit button
-        else if (ae.getSource() == submit) {
+        } else if (ae.getSource() == submit) {
             ans_given = 1;
             if (groupoptions.getSelection() == null) {
                 useranswers[count][0] = "";
             } else {
                 useranswers[count][0] = groupoptions.getSelection().getActionCommand();
             }
-            // Calculating score
+
             for (int i = 0; i < useranswers.length; i++) {
                 if (useranswers[i][0].equals(answers[i][1])) {
                     score += 10;
@@ -163,16 +217,15 @@ public class Quiz extends JFrame implements ActionListener {
                     score += 0;
                 }
             }
-            // Displaying score
             setVisible(false);
             new Score(name, score);
         }
     }
-    // Overriding paint method for timer and navigation
+    
     public void paint(Graphics g) {
         super.paint(g);
         
-        String time = "Time left - " + timer + " seconds";
+        String time = "Time left - " + timer + " seconds"; // 15
         g.setColor(Color.RED);
         g.setFont(new Font("Tahoma", Font.BOLD, 25));
         
@@ -182,7 +235,7 @@ public class Quiz extends JFrame implements ActionListener {
             g.drawString("Times up!!", 1100, 500);
         }
         
-        timer--;
+        timer--; // 14
         
         try {
             Thread.sleep(1000);
@@ -193,9 +246,9 @@ public class Quiz extends JFrame implements ActionListener {
         
         if (ans_given == 1) {
             ans_given = 0;
-            timer = 25;
+            timer = 15;
         } else if (timer < 0) {
-            timer = 25;
+            timer = 15;
             opt1.setEnabled(true);
             opt2.setEnabled(true);
             opt3.setEnabled(true);
@@ -205,7 +258,7 @@ public class Quiz extends JFrame implements ActionListener {
                 next.setEnabled(false);
                 submit.setEnabled(true);
             }
-            if (count == 9) {
+            if (count == 9) { // submit button
                 if (groupoptions.getSelection() == null) {
                    useranswers[count][0] = "";
                 } else {
@@ -221,19 +274,19 @@ public class Quiz extends JFrame implements ActionListener {
                 }
                 setVisible(false);
                 new Score(name, score);
-            } else {
+            } else { // next button
                 if (groupoptions.getSelection() == null) {
                    useranswers[count][0] = "";
                 } else {
                     useranswers[count][0] = groupoptions.getSelection().getActionCommand();
                 }
-                count++;
+                count++; // 0 // 1
                 start(count);
             }
         }
         
     }
-    // Method to display the question and options
+    
     public void start(int count) {
         qno.setText("" + (count + 1) + ". ");
         question.setText(questions[count][0]);
@@ -250,8 +303,8 @@ public class Quiz extends JFrame implements ActionListener {
         opt4.setActionCommand(questions[count][4]);
         
         groupoptions.clearSelection();
-    }    
-    // Main method to start the quiz
+    }
+    
     public static void main(String[] args) {
         new Quiz("User");
     }
